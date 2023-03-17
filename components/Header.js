39560acda60eb37594764ba628f77625930a965d
Yashsharma12345678 <91869549+Yashsharma12345678@users.noreply.github.com>
2023-03-17 19:37:34 +0530

    yashtv

---
+++ components/Header.js
@@ -0,0 +1,30 @@
+import Image from 'next/image'
+import React from 'react'
+import { CheckBadgeIcon , HomeIcon, BoltIcon , MagnifyingGlassIcon , UserIcon} from "@heroicons/react/24/outline" 
+import {RectangleStackIcon} from "@heroicons/react/24/solid" 
+
+import HeaderItem from './HeaderItem'
+
+function Header() {
+  return (
+    <>
+    <header className=' flex flex-col sm:flex-row m-5 justify-between items-center h-auto '>
+        <div className='flex flex-grow justify-evenly max-w-2xl'>
+            <HeaderItem title="Home" Icon={HomeIcon}  />
+            <HeaderItem title="Trending" Icon={BoltIcon}  />
+            <HeaderItem title="Verified" Icon={CheckBadgeIcon}  />
+            <HeaderItem title="Collection" Icon={RectangleStackIcon}  />
+            <HeaderItem title="Search" Icon={MagnifyingGlassIcon}  />
+            <HeaderItem title="Account" Icon={UserIcon}  />
+        </div>
+          <div>
+            <p className=' cursor-pointer flex flex-grow items-center  text-5xl px-4 tansition duration-200 ease-in transform sm:hover:scale-105 hover:z-50'>YashTV</p>
+          </div>
+    {/* <Image className='object-contain ' src="https://links.papareact.com/ua6" width={200} height={100}/> */}
+
+    </header>
+    </>
+  )
+}
+
+export default Header
---
+++ pages/index.js
@@ -0,0 +1,43 @@
+import Head from 'next/head'
+import Header from '../components/Header'
+import Navbar from '../components/Navbar'
+import Results from '../components/Results'
+import requests from '../utils/requests'
+
+export default function Home({results}) {
+
+  // console.log(props)
+  return (
+    <div>
+      <Head>
+        <title>Hulu </title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div>
+
+
+        {/* Header */}
+        <Header/>
+
+        {/* Navbar */}
+        <Navbar />
+
+        <Results results={results}/>
+
+      </div>
+    </div>
+  )
+}
+
+export async function getServerSideProps(context){
+  const genre = context.query.genre;
+
+  const request = await fetch(`https://api.themoviedb.org/3${requests[genre]?.url || requests.fetchTrending.url}`).then(res => res.json());
+
+  return {
+    props:{
+      results: request.results
+    }
+  }
+} 
